6a391563554a1a39ceeecd4811c08c529d739386
Yusuboy <Yuusufab@outlook.com>
2024-01-28 23:37:03 +0200

    fixed connection issues

---
+++ mern-exercise-tracker/backend/models/user.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlenght: 3
+    },
+},   {
+  timestamp: true,
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -10,13 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri);
+
 
 const connection = mongoose.connection;
 
-connection.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
-});
 
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
